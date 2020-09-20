37f4ab2c12724b648b5b939d469d54f0e174e3d5
Alcindo Schleder <alcindoschleder@gmail.com>
2020-09-19 23:10:41 -0300

    Criada a tela do admin para cadastro dos produtos

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import MarketProduct
 
-# Register your models here.
+
+class MarketProductAdmin(admin.ModelAdmin):
+    list_display = ('pk_product', 'product_description', 'product_stock')
+    list_filter = ('pk_product', 'product_description', 'product_stock')
+    search_fields = ('pk_product', 'product_description')
+
+
+admin.site.register(MarketProduct, MarketProductAdmin)
+
--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,17 @@
+# *-* coding: utf-8 *-*
+import textwrap
 from django.db import models
 
-# Create your models here.
+
+class MarketProduct(models.Model):
+    pk_product = models.CharField(max_length=50, verbose_name='Código', primary_key=True)
+    product_description = models.TextField('Descrição do Produto')
+    product_stock = models.FloatField('Estoque', default=0.0, null=True, blank=True)
+
+    class Meta:
+        db_table = 'market_product'
+        verbose_name = "Produtos"
+        verbose_name_plural = "Produtos"
+
+    def __str__(self):
+        return textwrap.shorten(self.product_description, width=50, placeholder='...')
